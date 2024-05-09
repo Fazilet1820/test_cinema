d93cffa26a37c4630954a0f2af5f9c87f6c7a1b6
Fazilet1820 <senolfazilet1820@gmail.com>
2024-05-09 18:37:21 +0200

    uppdated_cinema

--- reservation.js
+++ reservation.js
@@ -1,4 +1,30 @@
-
 function redirectToFormPage() {
   window.location.href = "form.html";
 }
+
+// DOM içeriği tamamen yüklendiğinde bu fonksiyon çalışır
+document.addEventListener("DOMContentLoaded", function () {
+  // 'seat' sınıfına sahip tüm elementleri seç
+  const seats = document.querySelectorAll(".seat");
+
+  // Her koltuğa sıralı olarak numara ekleme
+  let seatNumber = 1;
+  seats.forEach((seat) => {
+    seat.nextElementSibling.textContent = seatNumber;
+    seatNumber++;
+  });
+
+  // Her koltuğa 'click' olayı için bir olay dinleyici ekle
+  seats.forEach((seat) => {
+    seat.addEventListener("click", function () {
+      // Koltuğun işaretli olup olmadığını kontrol et
+      if (this.checked) {
+        // İşaretli ise, numarayı göster
+        this.nextElementSibling.style.visibility = "visible";
+      } else {
+        // İşaretli değilse, numarayı gizle
+        this.nextElementSibling.style.visibility = "hidden";
+      }
+    });
+  });
+});
